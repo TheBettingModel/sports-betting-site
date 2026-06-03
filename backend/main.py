721a559b23eae6106f81108a839bd6bc15a1d564
Jacques Kaune <jacqueskaune@Jacquess-MacBook-Pro.local>
2026-06-03 06:55:33 -0400

    Expose sharp sportsbook weighting fields

--- backend/main.py
+++ backend/main.py
@@ -4163,6 +4163,9 @@ def model_mlb_today():
                         plays.append({
                             "game": game_name,
                             "sportsbook": sportsbook,
+                            "sharp_book_score": sharp_book_data.get("sharp_book_score"),
+                            "sharp_book_signal": sharp_book_data.get("sharp_book_signal"),
+                            "book_weight_adjustment": book_weight_adjustment,
                             "market": market_name,
                             "pick": pick_name,
                             "odds": odds,
